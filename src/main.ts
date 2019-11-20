c235b211ae0584260a0d6db34fe0835fdc7df46b
Michael Carroll <michael@openrobotics.org>
2019-11-19 18:42:48 -0600

    Update for ignition
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,48 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import * as exec from '@actions/exec';
+import * as process from 'process';
+
+async function runAptGetInstall(packages: string[]): Promise<number> {
+  return exec.exec(
+    "sudo",
+    ["DEBIAN_FRONTEND=noninteractive",
+      "apt-get", "install", "--no-install-recommends", "--quiet",
+      "--yes"].concat(packages)
+  );
+}
+
+async function installPython3Dependencies(): Promise<number> {
+  return exec.exec(
+    "sudo",
+    ["pip3", "install", "--upgrade",
+      "colcon-common-extensions", "vcstool"]
+  );
+}
+
+async function runLinux() {
+  await exec.exec("sudo", ["apt-get", "update"]);
+
+  // Enforce UTC time for consistency.
+  await exec.exec("sudo", ["bash", "-c", "echo 'Etc/UTC' > /etc/timezone"]);
+  await exec.exec(
+      "sudo",
+      ["ln", "-sf", "/usr/share/zoneinfo/Etc/UTC", "/etc/localtime"])
+  await runAptGetInstall(["tzdata"]);
+  await runAptGetInstall(
+    ["build-essential", "clang", "cmake", "cppcheck", 
+      "git", "lcov", "pkg-config", "wget",
+      "python3-setuptools"]);
+  await installPython3Dependencies();
+}
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    const platform = process.platform;
+    if (platform === 'linux') {
+      await runLinux();
+    } else {
+      core.setFailed(`unsupported platform ${platform}`);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
